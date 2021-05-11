140fa0b9a0a81c3d3f46247d656739ed5a3f401d
Ekaterina Ledeneva <ekiuled@gmail.com>
2021-05-11 04:24:18 +0300

    Negative tests

--- PascalLexer.Tests/PascalLexerTests.cs
+++ PascalLexer.Tests/PascalLexerTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using NUnit.Framework;
 
@@ -6,38 +7,67 @@ namespace PascalLexer.Tests
 {
     public class Tests
     {
+        private static void AssertTokenType(List<string> inputs, int tokenType)
+        {
+            inputs.ForEach(input => CollectionAssert.AreEqual(
+                new[] {tokenType, TokenType.Eof},
+                Lexer.Lex(input).Select(t => t.Type)));
+        }
+
+        private static void AssertNotTokenType(List<string> inputs, int tokenType)
+        {
+            inputs.ForEach(input =>
+            {
+                try
+                {
+                    CollectionAssert.AreNotEqual(
+                        new[] {tokenType, TokenType.Eof},
+                        Lexer.Lex(input).Select(t => t.Type));
+                }
+                catch (ArgumentException)
+                {
+                }
+            });
+        }
+
         [Test]
         public void TestNumbers()
         {
-            new List<string>
+            AssertTokenType(new List<string>
             {
                 "123", "$abCd00eF2F1", "&17", "%111110100011", "0.313E1", "-0", "+%11"
-            }.ForEach(input => CollectionAssert.AreEqual(
-                new[] {TokenType.Number, TokenType.Eof},
-                Lexer.Lex(input).Select(t => t.Type)));
+            }, TokenType.Number);
+            AssertNotTokenType(new List<string>
+            {
+                "+", "$", "&", "%", ".", "-", ".123", "NaN", "$0FG", "%2", "&8", "123E"
+            }, TokenType.Number);
         }
 
         [Test]
         public void TestIdentifiers()
         {
-            new List<string>
+            AssertTokenType(new List<string>
             {
                 "_", "______", "a", "asd_asdnlUUUUbebnn1232313_", "_123", "g112"
-            }.ForEach(input => CollectionAssert.AreEqual(
-                new[] {TokenType.Identifier, TokenType.Eof},
-                Lexer.Lex(input).Select(t => t.Type)));
+            }, TokenType.Identifier);
+            AssertNotTokenType(new List<string>
+            {
+                "123", ".", "_df?f"
+            }, TokenType.Identifier);
         }
 
         [Test]
         public void TestCharacterStrings()
         {
-            new List<string>
+            AssertTokenType(new List<string>
             {
                 "''", "''''", "'This is a pascal string'", "'A tabulator character: '#9' is easy to embed'",
                 "'the string starts here'#13#10'   and continues here'"
-            }.ForEach(input => CollectionAssert.AreEqual(
-                new[] {TokenType.CharacterString, TokenType.Eof},
-                Lexer.Lex(input).Select(t => t.Type)));
+            }, TokenType.CharacterString);
+            AssertNotTokenType(new List<string>
+            {
+                "'''", "'\\''", "'the string starts here \n and continues here'"
+            }, TokenType.CharacterString);
         }
     }
 }
--- PascalLexer/Lexer.cs
+++ PascalLexer/Lexer.cs
@@ -1,13 +1,27 @@
+using System;
 using System.Collections.Generic;
+using System.IO;
 using Antlr4.Runtime;
 
 namespace PascalLexer
 {
     public static class Lexer
     {
-        public static IList<IToken> Lex(string input)
+        private class ThrowExceptionErrorListener : BaseErrorListener, IAntlrErrorListener<int>
         {
-            var tokens = new CommonTokenStream(new Grammar(new AntlrInputStream(input)));
+            public void SyntaxError(TextWriter output, IRecognizer recognizer, int offendingSymbol, int line,
+                int charPositionInLine,
+                string msg, RecognitionException e)
+            {
+                throw new ArgumentException("Lexer error", msg, e);
+            }
+        }
+
+        public static IEnumerable<IToken> Lex(string input)
+        {
+            var grammar = new Grammar(new AntlrInputStream(input));
+            grammar.AddErrorListener(new ThrowExceptionErrorListener());
+            var tokens = new CommonTokenStream(grammar);
             tokens.Fill();
             return tokens.GetTokens();
         }
